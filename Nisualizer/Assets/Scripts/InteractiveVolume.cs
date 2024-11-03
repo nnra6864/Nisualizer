597fd757856b88c70f0f20fa1643c8a31cd0dd32
nnra6864 <enenragd@gmail.com>
2024-11-03 15:02:21 +0100

    Using opposite noise for wind to make it smoother and implemented more post processing effects

--- Nisualizer/Assets/Scripts/InteractiveVolume.cs
+++ Nisualizer/Assets/Scripts/InteractiveVolume.cs
@@ -14,6 +14,11 @@ public class InteractiveVolume : MonoBehaviour
     private Vignette _vignette;
     [SerializeField] private Vector2 _vignetteRange = new(0.25f, 0.3f);
     
+    private ColorAdjustments _colorAdjustments;
+    [SerializeField] private Vector2 _postExpoRange = new(0, 0.5f);
+    [SerializeField] private Vector2 _contrastRange = new(0, 10);
+    [SerializeField] private Vector2 _saturationRange= new(0, 25);
+    
     private void Reset()
     {
         _microphoneData = FindFirstObjectByType<MicrophoneDataScript>();
@@ -24,11 +29,15 @@ public class InteractiveVolume : MonoBehaviour
     {
         _volume.profile.TryGet(out _lensDistortion);
         _volume.profile.TryGet(out _vignette);
+        _volume.profile.TryGet(out _colorAdjustments);
     }
 
     private void Update()
     {
-        _lensDistortion.intensity.value = Mathf.LerpUnclamped(_lensDistortionRange.x, _lensDistortionRange.y, _microphoneData.Loudness);
-        _vignette.intensity.value       = Mathf.LerpUnclamped(_vignetteRange.x, _vignetteRange.y, _microphoneData.Loudness);
+        _lensDistortion.intensity.value      = Mathf.LerpUnclamped(_lensDistortionRange.x, _lensDistortionRange.y, _microphoneData.Loudness);
+        _vignette.intensity.value            = Mathf.LerpUnclamped(_vignetteRange.x, _vignetteRange.y, _microphoneData.Loudness);
+        _colorAdjustments.postExposure.value = Mathf.LerpUnclamped(_postExpoRange.x, _postExpoRange.y, _microphoneData.Loudness);
+        _colorAdjustments.contrast.value     = Mathf.LerpUnclamped(_contrastRange.x, _contrastRange.y, _microphoneData.Loudness);
+        _colorAdjustments.saturation.value   = Mathf.LerpUnclamped(_saturationRange.x, _saturationRange.y, _microphoneData.Loudness);
     }
 }
