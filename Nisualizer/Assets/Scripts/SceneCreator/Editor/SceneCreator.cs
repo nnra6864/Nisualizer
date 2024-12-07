222cf4e2a3ad2df16ce5c0c3b474a5c4b8461d24
nnra6864 <enenragd@gmail.com>
2024-12-07 08:17:48 +0100

    Added scene asset creation

--- Nisualizer/Assets/Scripts/SceneCreator/Editor/SceneCreator.cs
+++ Nisualizer/Assets/Scripts/SceneCreator/Editor/SceneCreator.cs
@@ -1,5 +1,6 @@
 using System.IO;
 using UnityEditor;
+using UnityEditor.SceneManagement;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -42,27 +43,29 @@ namespace SceneCreator.Editor
 
         private void CreateScene()
         {
-            // Store scene name
+            // Store scene name, dir and path
             var sceneName = _sceneNameField.text;
+            var sceneDir = Path.Combine("Assets/Scenes", sceneName);
+            var scenePath = Path.Combine(sceneDir, sceneName) + ".unity";
             
-            // Get the scene path
-            var scenePath = Path.Combine("Assets/Scenes", sceneName);
-            
-            if (!CreateSceneDirectory(sceneName, scenePath)) return;
-            
-        }
-
-        private bool CreateSceneDirectory(string sceneName, string scenePath)
-        {
             // Return if scene name is empty
             if (string.IsNullOrEmpty(sceneName))
             {
                 Debug.LogError("Scene name can't be empty");
-                return false;
+                return;
             }
             
+            // Create scene dir
+            if (!CreateSceneDirectory(sceneName, sceneDir)) return;
+            
+            // Create scene asset
+            CreateSceneAsset(scenePath);
+        }
+
+        private bool CreateSceneDirectory(string sceneName, string sceneDir)
+        {
             // Check if the scene with that name already exists
-            if (AssetDatabase.AssetPathExists(scenePath))
+            if (AssetDatabase.AssetPathExists(sceneDir))
             {
                 Debug.LogError($"A scene directory named '{sceneName}' already exists");
                 return false;
@@ -71,6 +74,12 @@ namespace SceneCreator.Editor
             // Create the scene directory
             AssetDatabase.CreateFolder("Assets/Scenes", sceneName);
             return true;
+        }
+
+        private void CreateSceneAsset(string scenePath)
+        {
+            var newScene = EditorSceneManager.NewScene(NewSceneSetup.EmptyScene);
+            EditorSceneManager.SaveScene(newScene, scenePath);
         }
     }
 }
