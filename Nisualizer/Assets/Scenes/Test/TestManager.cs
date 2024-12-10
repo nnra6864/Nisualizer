1aa000fc5110faaa687b68c37ce928974b4531c5
nnra6864 <enenragd@gmail.com>
2024-12-10 21:51:38 +0100

    Changed the Snowstorm scene to match the rest

--- Nisualizer/Assets/Scenes/Test/TestManager.cs
+++
@@ -1,9 +0,0 @@
-using Core;
-
-namespace Scenes.Test
-{
-    public class TestManager : SceneScript
-    {
-        public static TestConfigData ConfigData => (TestConfigData)Config.Data;
-    }
-}
--- Nisualizer/Assets/Scenes/Test1/Test1Manager.cs
+++
@@ -1,9 +0,0 @@
-using Core;
-
-namespace Scenes.Test1
-{
-    public class Test1Manager : SceneScript
-    {
-        public static Test1ConfigData ConfigData => (Test1ConfigData)Config.Data;
-    }
-}
