d9c03163a7356734fab3745f789cd47970f60159
nnra6864 <enenragd@gmail.com>
2024-12-10 23:07:12 +0100

    Implemented the InteractiveVideoPlayer

---
+++ Nisualizer/Assets/Scripts/InteractiveComponents/InteractiveVideoPlayer.cs
@@ -0,0 +1,27 @@
+using Core;
+using UnityEngine;
+using UnityEngine.Video;
+
+namespace InteractiveComponents
+{
+    [RequireComponent(typeof(VideoPlayer))]
+    public class InteractiveVideoPlayer : MonoBehaviour
+    {
+        private static MicrophoneDataScript MicrophoneData => GameManager.MicrophoneData;
+
+        [SerializeField] private VideoPlayer _videoPlayer;
+        
+        [Tooltip("Set to 1, 1 for no effect")]
+        [SerializeField] private Vector2 _playbackSpeedRange = new(1, 1);
+        
+        private void Reset()
+        {
+            _videoPlayer = GetComponent<VideoPlayer>();
+        }
+
+        private void Update()
+        {
+            _videoPlayer.playbackSpeed = Mathf.LerpUnclamped(_playbackSpeedRange.x, _playbackSpeedRange.y, MicrophoneData.Loudness);
+        }
+    }
+}
