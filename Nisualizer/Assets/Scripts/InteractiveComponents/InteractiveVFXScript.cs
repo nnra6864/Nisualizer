748f6d3fe3b973c76b4c6fa7eb61ccb3484fb621
nnra6864 <enenragd@gmail.com>
2024-12-11 15:59:47 +0100

    Made play rate change for interactive vfx optional

--- Nisualizer/Assets/Scripts/InteractiveComponents/InteractiveVFXScript.cs
+++ Nisualizer/Assets/Scripts/InteractiveComponents/InteractiveVFXScript.cs
@@ -17,6 +17,9 @@ namespace InteractiveComponents
         [Tooltip("Set to 1, 1 for no effect")]
         [SerializeField] private Vector2 _playRateRange = new(1, 1);
 
+        [Tooltip("Whether the play rate of the VFX will be affected")]
+        [SerializeField] private bool _affectPlayRate = true;
+
         private void Reset()
         {
             _vfx = GetComponent<VisualEffect>();
@@ -24,8 +27,8 @@ namespace InteractiveComponents
 
         private void Update()
         {
-            _vfx.playRate = Mathf.LerpUnclamped(_playRateRange.x, _playRateRange.y, MicrophoneData.Loudness);
             _vfx.SetFloat(_loudnessPropertyName, MicrophoneData.Loudness);
+            if (_affectPlayRate) _vfx.playRate = Mathf.LerpUnclamped(_playRateRange.x, _playRateRange.y, MicrophoneData.Loudness);
         }
     }
 }
