e76d446d6438082b3285d0ba1724fc3856c63cab
nnra6864 <enenragd@gmail.com>
2024-12-13 01:11:04 +0100

    Implemented ConfigGradientColorKey

--- Nisualizer/Assets/Scripts/Config/Types/ConfigGradientColorKey.cs
+++ Nisualizer/Assets/Scripts/Config/Types/ConfigGradientColorKey.cs
@@ -4,6 +4,34 @@ namespace Config.Types
 {
     public class ConfigGradientColorKey
     {
+        public ConfigColor Color;
+        public float Time;
 
+        /// Default ctor
+        public ConfigGradientColorKey()
+        {
+            Color = new();
+            Time  = 0;
+        }
+
+        /// Ctor from values
+        public ConfigGradientColorKey(ConfigColor color, float time)
+        {
+            Color = color;
+            Time  = time;
+        }
+
+        /// Ctor from <see cref="GradientAlphaKey"/>
+        public ConfigGradientColorKey(GradientColorKey key)
+        {
+            Color = key.color;
+            Time  = key.time;
+        }
+
+        /// Implicit operator from <see cref="GradientAlphaKey"/>
+        public static implicit operator ConfigGradientColorKey(GradientColorKey key) => new(key);
+
+        /// Implicit operator to <see cref="GradientAlphaKey"/>
+        public static implicit operator GradientColorKey(ConfigGradientColorKey key) => new(key.Color, key.Time);
     }
 }
