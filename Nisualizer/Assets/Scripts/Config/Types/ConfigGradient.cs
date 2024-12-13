dd861d812809643fb9310189034454f9be179cd7
nnra6864 <enenragd@gmail.com>
2024-12-13 01:26:14 +0100

    Implemented ConfigGradient

--- Nisualizer/Assets/Scripts/Config/Types/ConfigGradient.cs
+++ Nisualizer/Assets/Scripts/Config/Types/ConfigGradient.cs
@@ -1,9 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace Config.Types
 {
-    public class ConfigGradient : MonoBehaviour
+    [Serializable]
+    public class ConfigGradient
     {
-        
+        public List<ConfigGradientAlphaKey> AlphaKeys;
+        public List<ConfigGradientColorKey> ColorKeys;
+
+        public ConfigGradient()
+        {
+            AlphaKeys = new();
+            ColorKeys = new();
+        }
+
+        public ConfigGradient(Gradient gradient)
+        {
+            // Using Select() because it supports implicit conversion
+            AlphaKeys = new(gradient.alphaKeys.Select(key => (ConfigGradientAlphaKey)key));
+            ColorKeys = new(gradient.colorKeys.Select(key => (ConfigGradientColorKey)key));
+        }
+
+        /// Implicit conversion from <see cref="Gradient"/>
+        public static implicit operator ConfigGradient(Gradient g) => new(g);
+
+        /// Implicit conversion to <see cref="Gradient"/>
+        public static implicit operator Gradient(ConfigGradient g) => new()
+        {
+            alphaKeys = g.AlphaKeys.Select(key => (GradientAlphaKey)key).ToArray(),
+            colorKeys = g.ColorKeys.Select(key => (GradientColorKey)key).ToArray()
+        };
     }
 }
--- Nisualizer/Assets/Scripts/Config/Types/ConfigGradientColorKey.cs
+++ Nisualizer/Assets/Scripts/Config/Types/ConfigGradientColorKey.cs
@@ -1,7 +1,9 @@
+using System;
 using UnityEngine;
 
 namespace Config.Types
 {
+    [Serializable]
     public class ConfigGradientColorKey
     {
         public ConfigColor Color;
